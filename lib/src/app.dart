a8a9a9f4c819408a41b1b186fc0d246b84e7ea91
herovickers <victoreronmosele@gmail.com>
2019-01-11 12:32:00 +0100

    set preferred orientation to portrait up

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'ui/home_widget.dart';
 
 class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    
+     @override
+    Widget build(BuildContext context) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);    
     return MaterialApp(
         title: "Outfit Battle",
         home: Home(),
