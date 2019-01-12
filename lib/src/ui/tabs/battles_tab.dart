2b35a81be1059203c908c74340e1d8256ec32a5f
herovickers <victoreronmosele@gmail.com>
2019-01-12 23:16:12 +0100

    add placeholder images to battle box

--- lib/src/ui/tabs/battles_tab.dart
+++ lib/src/ui/tabs/battles_tab.dart
@@ -6,12 +6,15 @@ class BattlesTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String firstBattlerImageUrl = "https://loremflickr.com/300/400/model,girl?lock=37";
+    String secondBattlerImageUrl = "https://loremflickr.com/300/400/model,girl?lock=5";
+
     return Scaffold(
       body: Column(
         children: <Widget>[
           buildTitleBar(),
           Spacer(),
-          buildBattleBox(),
+          buildBattleBox(firstBattlerImageUrl, secondBattlerImageUrl),
           Spacer(),
           buildBattleButton(),
         ],
@@ -22,22 +25,6 @@ class BattlesTab extends StatelessWidget {
   Expanded buildBattleButton() {
     return Expanded(
       flex: 2,
-      // child: Center(
-      //   child: 
-      //        RaisedButton.icon(
-      //         onPressed: () {},
-      //         color: Colors.black,
-      //         elevation: 4.0,
-      //         shape: new RoundedRectangleBorder(
-      //             borderRadius: new BorderRadius.circular(30.0)),
-      //         label: Text("Battle", style: TextStyle(color: Colors.white),),
-      //         icon: Padding(
-      //           padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-      //           child: Icon(CustomIcons.swords_crossed, 
-      //           color: Colors.white,),
-      //         ),
-      //          ),
-      // ),
         child : FloatingActionButton(
           foregroundColor: Colors.white,
           backgroundColor: Colors.black,
@@ -48,12 +35,10 @@ class BattlesTab extends StatelessWidget {
               ),
           onPressed: (){},
         )
-      // ...FloatingActionButton properties...
-    // ),
     );
   }
 
-  Expanded buildBattleBox() {
+  Expanded buildBattleBox(String firstBattlerImageUrl, String secondBattlerImageUrl) {
     return Expanded(
       flex: 14,
       child: Card(
@@ -63,7 +48,7 @@ class BattlesTab extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 buildUserIdentities(),
-                buildBattlePreviewLayout(),
+                buildBattlePreviewLayout(firstBattlerImageUrl, secondBattlerImageUrl),
                 buildHashtagsLayout(),
               ],
             ),
@@ -71,93 +56,61 @@ class BattlesTab extends StatelessWidget {
     );
   }
 
-  Padding buildHashtagsLayout() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child:
-              Text("#fridayChilling"),
-              ),
-          Expanded(
-            flex: 1,
-            child:
-              Text("#fridayChilling"),              
-              ),
-      ],
-    ),
-    );
-  }
-
-  Expanded buildBattlePreviewLayout() {
+Expanded buildUserIdentities() {
     return Expanded(
-                flex: 1,
+                flex: 2,
                 child: Center(
                   child: (
-                    Container(color: Colors.limeAccent,)
+                    Container(color: Colors.deepPurpleAccent)
                   ),
                 ),
               );
   }
 
-  Padding buildUserIdentities() {
-    return Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+  Expanded buildBattlePreviewLayout(String firstBattlerImageUrl, String secondBattlerImageUrl) {
+    return Expanded(
+                flex: 6,
                 child: Row(
-                  children: <Widget>[
-  new Expanded(
-    flex: 1,
-    child:
-                          Row(  
-                          children: <Widget>[
-                            new Container(
-                              height: 40.0,
-                              width: 40.0,
-                              decoration: new BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: new DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: new NetworkImage(
-                                        "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
-                              ),
-                            ),
-                            new SizedBox(
-                              width: 10.0,
-                            ),
-                            new Text(
-                              "janeDoe",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),),
-                         new Expanded(
-    flex: 1,
-    child:
-                          Row(  
-                          children: <Widget>[
-                            new Container(
-                              height: 40.0,
-                              width: 40.0,
-                              decoration: new BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: new DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: new NetworkImage(
-                                        "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
-                              ),
-                            ),
-                            new SizedBox(
-                              width: 10.0,
-                            ),
-                            new Text(
-                              "johnDoe",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),),
-],
+                  children: imagesRow(firstBattlerImageUrl, secondBattlerImageUrl),
+                  ),
+              );
+  }
+
+  List<Widget> imagesRow(String firstBattlerImageUrl, String secondBattlerImageUrl) {
+    return <Widget>[
+                    buildImageCard(firstBattlerImageUrl),
+                    buildImageCard(secondBattlerImageUrl),
+                ];
+  }
+
+  Expanded buildImageCard(String battlerImageUrl) {
+    return Expanded(
+                    flex: 1,
+                    child: Card(
+                      child: AspectRatio(
+          aspectRatio: 3 / 4,
+          child: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                fit: BoxFit.fitWidth,
+                alignment: FractionalOffset.topCenter,
+                image: new NetworkImage(battlerImageUrl),
+              )
+            ),
+          ),
+        ),
+                    
+                  ),
+    );
+  }
+
+  Expanded buildHashtagsLayout() {
+    return Expanded(
+                flex: 2,
+                child: Center(
+                  child: (
+                    Container(color: Colors.deepPurpleAccent)
+                  ),
                 ),
               );
   }
