cf519f8d633c997b3d73a539956454a434d8fdb3
herovickers <victoreronmosele@gmail.com>
2019-01-12 01:27:09 +0100

    change button colour to black and white

--- lib/src/ui/tabs/battles_tab.dart
+++ lib/src/ui/tabs/battles_tab.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:outfit_battle/src/resources/custom_icons.dart';
 
 class BattlesTab extends StatelessWidget {
-
   BattlesTab();
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Column(
         children: <Widget>[
           buildTitleBar(),
@@ -18,56 +17,48 @@ class BattlesTab extends StatelessWidget {
         ],
       ),
     );
- }
+  }
 
- Expanded buildBattleButton() {
-   return Expanded(
-          flex: 3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton.icon(
-                onPressed: (){},
-                color: Colors.yellow,
-                 elevation: 4.0,
-                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                label: 
-                    Text("Battle"), 
-                    icon: Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-                      child: Icon(CustomIcons.swords_crossed),
-                    ),
-        ),
-        
-            ],
-          ),
-        );
- }
-
- Expanded buildBattleBox() {
-   return Expanded(
-          flex: 13,
-          child: Container(
-            color: Colors.blue,
-          ),
-        );
- }
-
- Expanded buildTitleBar() {
-   return Expanded(
-        flex: 2,
-          child: 
-              Row(
-                children: <Widget>[
-                  Text(
-                   "Battles",
-                   style: TextStyle(
-                     fontFamily: 'Amatic',
-                     fontSize: 32.0
-                   )
-                  ),
-                ],
+  Expanded buildBattleButton() {
+    return Expanded(
+      flex: 2,
+      child: Center(
+        child: 
+             RaisedButton.icon(
+              onPressed: () {},
+              color: Colors.black,
+              elevation: 4.0,
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)),
+              label: Text("Battle", style: TextStyle(color: Colors.white),),
+              icon: Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+                child: Icon(CustomIcons.swords_crossed, 
+                color: Colors.white,),
               ),
-        );
- }
+               ),
+      ),
+    );
+  }
+
+  Expanded buildBattleBox() {
+    return Expanded(
+      flex: 14,
+      child: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  Expanded buildTitleBar() {
+    return Expanded(
+      flex: 2,
+      child: Row(
+        children: <Widget>[
+          Text("Battle",
+              style: TextStyle(fontFamily: 'Amatic', fontSize: 32.0)),
+        ],
+      ),
+    );
+  }
 }
