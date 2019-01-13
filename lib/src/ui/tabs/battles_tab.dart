187ddc6518cb82b2d93bf22c51d0cd6875fc732f
herovickers <victoreronmosele@gmail.com>
2019-01-13 08:15:01 +0100

    add next and previous buttons to battle layout

--- lib/src/ui/tabs/battles_tab.dart
+++ lib/src/ui/tabs/battles_tab.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:outfit_battle/src/resources/custom_icons.dart';
 import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
 import 'package:flutter_advanced_networkimage/transition_to_image.dart';
-import 'package:flutter_advanced_networkimage/zoomable_list.dart';
-
 
 class BattlesTab extends StatelessWidget {
   BattlesTab();
@@ -37,7 +35,8 @@ class BattlesTab extends StatelessWidget {
                 child: Icon(CustomIcons.swords_crossed, 
                 color: Colors.white,),
               ),
-          onPressed: (){},
+          onPressed: (){
+          },
         )
     );
   }
@@ -74,46 +73,106 @@ Expanded buildUserIdentities() {
   Expanded buildBattlePreviewLayout(String firstBattlerImageUrl, String secondBattlerImageUrl) {
     return Expanded(
                 flex: 6,
-                child: Row(
-                  children: imagesRow(firstBattlerImageUrl, secondBattlerImageUrl),
-                  ),
-              );
+                child: Stack(
+                  children: <Widget>[
+                    imagesRow(firstBattlerImageUrl, secondBattlerImageUrl),
+                    votingLayout(),
+                    previousButton(),
+                    nextButton(),
+                  ],
+                ),
+    );
   }
 
-  List<Widget> imagesRow(String firstBattlerImageUrl, String secondBattlerImageUrl) {
-    return <Widget>[
+  Align previousButton() {
+    return Align(
+      alignment: Alignment(-1.1, 0.0),
+                      child: Container(
+                        color: Colors.black45,
+                        child: IconButton(
+                           color: Colors.white70,
+                          icon: Icon(Icons.navigate_before),
+                          onPressed: (){},
+                        ),),
+                      
+                    );
+  }
+  
+  Align nextButton() {
+    return Align(
+      alignment: Alignment(1.1, 0.0),
+                      child: Container(
+                        color: Colors.black45,
+                        child: IconButton(
+                           color: Colors.white70,
+                          icon: Icon(Icons.navigate_next),
+                          onPressed: (){},
+                        ),),
+                      
+                    );
+  }
+  
+  
+
+  Container votingLayout() {
+    return Container(
+                      constraints: BoxConstraints(
+                          maxHeight: double.infinity,
+                          maxWidth: double.infinity,
+                          minWidth: double.infinity,
+                          minHeight: double.infinity
+                      ),
+                      child: FractionallySizedBox(
+                          alignment: Alignment(0.0, 0.9),
+                          widthFactor: 0.4,
+                          heightFactor: 0.2,
+                          child: Container(
+                            color: Colors.green,
+                          ),
+                        ),
+                    );
+  }
+
+  Row imagesRow(String firstBattlerImageUrl, String secondBattlerImageUrl) {
+    return Row(
+                  children: <Widget>[
                     buildImageCard(firstBattlerImageUrl),
                     buildImageCard(secondBattlerImageUrl),
-                ];
+                ],           
+     ) ;
   }
 
   Expanded buildImageCard(String battlerImageUrl) {
     return Expanded(
                     flex: 1,
-                    child: Card(
-                      child: AspectRatio(
-          aspectRatio: 3 / 4,
-          child: new Container(
-            child: TransitionToImage(
-                  AdvancedNetworkImage(battlerImageUrl,
-                    loadedCallback: () {
-                      print('It works!');
-                    },
-                    loadFailedCallback: () {
-                      print('Oh, no!');
-                    },
-                    useDiskCache: true,
-                  ),
-                  loadingWidget: const CircularProgressIndicator(),
-                  fit: BoxFit.contain,
-                  placeholder: const Icon(Icons.refresh),
-                  enableRefresh: true,
-                ),
-                
-              //NetworkImage(battlerImageUrl),
-            ),
-          ),
-        ),
+                    child: Center(
+                    child: AspectRatio(
+                          aspectRatio: 3 / 4,
+                    child: Card(    
+                          child: new Container(
+                            child: TransitionToImage(
+                                  AdvancedNetworkImage(battlerImageUrl,
+                                    loadedCallback: () {
+                                      // showVoteButton();
+                                      print('Image loaded!');
+                                    },
+                                    loadFailedCallback: () {
+                                      // hideVoteButton();
+                                      print('Image load failed!');
+                                    },
+                                    useDiskCache: true,
+                                  ),
+                                  loadingWidget: const CircularProgressIndicator(),
+                                  fit: BoxFit.cover,
+                                  placeholder: const Icon(Icons.refresh),
+                                  enableRefresh: true,
+                                ),
+                                
+                              //NetworkImage(battlerImageUrl),
+                            ),
+                          ),
+                      ),
+                      ),
     );
   }
 
