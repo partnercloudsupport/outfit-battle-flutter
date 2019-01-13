92522b6f40eca65d6ad45e0177c4cba7d6b9a7ff
herovickers <victoreronmosele@gmail.com>
2019-01-13 17:17:05 +0100

    refactor battle avatar to different dart file

--- lib/src/ui/tabs/battles_tab.dart
+++ lib/src/ui/tabs/battles_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:outfit_battle/src/resources/custom_icons.dart';
 import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
 import 'package:flutter_advanced_networkimage/transition_to_image.dart';
+import 'package:outfit_battle/src/ui/widgets/battler_avatar.dart';
 
 class BattlesTab extends StatelessWidget {
   BattlesTab();
@@ -321,74 +322,3 @@ class BattlesTab extends StatelessWidget {
   }
 }
 
-// class BattlerAvatar extends StatelessWidget {
-//    BattlerAvatar(String battlerProfilePictureUrl, {Key key,this.radius,
-//     this.minRadius,
-//     this.maxRadius,}) 
-//     : assert(radius == null || (minRadius == null && maxRadius == null)),
-//         super(key: key);
-
-Widget buildBattlerAvatar(String battlerProfilePictureUrl, {String battlerNumber, radius,
-    minRadius,
-    maxRadius,})
-{
- double radius;
- double minRadius;
- double maxRadius;
-  const double _defaultRadius = 20.0;
-  const double _defaultMinRadius = 0.0;
-  const double _defaultMaxRadius = double.infinity;
-
-double getMinDiameter() {
-    if (radius == null && minRadius == null && maxRadius == null) {
-      return _defaultRadius * 2.0;
-    }
-    return 2.0 * (radius ?? minRadius ?? _defaultMinRadius);
-  }
-
-  double getMaxDiameter() {
-    if (radius == null && minRadius == null && maxRadius == null) {
-      return _defaultRadius * 2.0;
-    }
-    return 2.0 * (radius ?? maxRadius ?? _defaultMaxRadius);
-  }  
-
-    return Container(
-    constraints: BoxConstraints(
-        minHeight: getMinDiameter(),
-        minWidth: getMinDiameter(),
-        maxWidth: getMaxDiameter(),
-        maxHeight: getMaxDiameter(),
-      ),
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-    ),
-    child: ClipOval(
-          child: TransitionToImage(
-         AdvancedNetworkImage(
-          battlerProfilePictureUrl,
-           loadedCallback: () {
-             // showVoteButton();
-             print('Image loaded!');
-           },
-           loadFailedCallback: () {
-             // hideVoteButton();
-             print('Image load failed!');
-           },
-           useDiskCache: true,
-         ),
-         loadingWidget: const CircularProgressIndicator(
-           valueColor: AlwaysStoppedAnimation<Color>(Colors.black45),
-         ),
-         fit: BoxFit.cover,
-         placeholder:  CircleAvatar(
-           backgroundColor: Colors.black54,
-           child: Text(battlerNumber,
-           style: TextStyle(color: Colors.white,),
-         ),
-         ),
-      enableRefresh: true,),
-       
-       ),
-    );
-  }
---
+++ lib/src/ui/widgets/battler_avatar.dart
@@ -0,0 +1,68 @@
+import "package:flutter/material.dart";
+import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
+import 'package:flutter_advanced_networkimage/transition_to_image.dart';
+
+Widget buildBattlerAvatar(String battlerProfilePictureUrl, {String battlerNumber, radius,
+    minRadius,
+    maxRadius,})
+{
+ double radius;
+ double minRadius;
+ double maxRadius;
+  const double _defaultRadius = 20.0;
+  const double _defaultMinRadius = 0.0;
+  const double _defaultMaxRadius = double.infinity;
+
+double getMinDiameter() {
+    if (radius == null && minRadius == null && maxRadius == null) {
+      return _defaultRadius * 2.0;
+    }
+    return 2.0 * (radius ?? minRadius ?? _defaultMinRadius);
+  }
+
+  double getMaxDiameter() {
+    if (radius == null && minRadius == null && maxRadius == null) {
+      return _defaultRadius * 2.0;
+    }
+    return 2.0 * (radius ?? maxRadius ?? _defaultMaxRadius);
+  }  
+
+    return Container(
+    constraints: BoxConstraints(
+        minHeight: getMinDiameter(),
+        minWidth: getMinDiameter(),
+        maxWidth: getMaxDiameter(),
+        maxHeight: getMaxDiameter(),
+      ),
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+    ),
+    child: ClipOval(
+          child: TransitionToImage(
+         AdvancedNetworkImage(
+          battlerProfilePictureUrl,
+           loadedCallback: () {
+             // showVoteButton();
+             print('Image loaded!');
+           },
+           loadFailedCallback: () {
+             // hideVoteButton();
+             print('Image load failed!');
+           },
+           useDiskCache: true,
+         ),
+         loadingWidget: const CircularProgressIndicator(
+           valueColor: AlwaysStoppedAnimation<Color>(Colors.black45),
+         ),
+         fit: BoxFit.cover,
+         placeholder:  CircleAvatar(
+           backgroundColor: Colors.black54,
+           child: Text(battlerNumber,
+           style: TextStyle(color: Colors.white,),
+         ),
+         ),
+      enableRefresh: true,),
+       
+       ),
+    );
+  }
