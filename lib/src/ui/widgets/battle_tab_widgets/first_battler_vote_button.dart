3f2617cebcd144480065984023c09f191b8461e2
herovickers <victoreronmosele@gmail.com>
2019-01-19 00:21:09 +0100

    change voting button bacakground to transparent

--- lib/src/ui/widgets/battle_tab_widgets/first_battler_vote_button.dart
+++ lib/src/ui/widgets/battle_tab_widgets/first_battler_vote_button.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
 Container buildFirstBattlerVoteButton() {
-    return Container(
-      decoration: new BoxDecoration(
-        border: Border.all(
-          color: Colors.white
-        ),
-        color: Colors.transparent,
-        shape: BoxShape.circle,
-      ),
-      child: IconButton(
-        splashColor: Colors.white,
-        color: Colors.white,
-        icon: Icon(Icons.check),
-        onPressed: () {},
-      ),
-    );
-  }
+  return Container(
+    decoration: new BoxDecoration(
+      border: Border.all(color: Colors.white),
+      color: Colors.transparent,
+      shape: BoxShape.circle,
+    ),
+    child: IconButton(
+      splashColor: Colors.white,
+      color: Colors.white,
+      icon: Icon(Icons.check),
+      onPressed: () {},
+    ),
+  );
+}
--- lib/src/ui/widgets/battle_tab_widgets/second_battler_vote_button.dart
+++ lib/src/ui/widgets/battle_tab_widgets/second_battler_vote_button.dart
@@ -1,19 +1,17 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 Container buildSecondBattlerVoteButton() {
-    return Container(
-      decoration: new BoxDecoration(
-        border: Border.all(
-          color: Colors.white
-        ),
-        color: Colors.transparent,
-        shape: BoxShape.circle,
-      ),
-      child: IconButton(
-        splashColor: Colors.white,
-        color: Colors.white,
-        icon: Icon(Icons.check),
-        onPressed: () {},
-      ),
-    );
-  }
+  return Container(
+    decoration: new BoxDecoration(
+      border: Border.all(color: Colors.white),
+      color: Colors.transparent,
+      shape: BoxShape.circle,
+    ),
+    child: IconButton(
+      splashColor: Colors.white,
+      color: Colors.white,
+      icon: Icon(Icons.check),
+      onPressed: () {},
+    ),
+  );
+}
