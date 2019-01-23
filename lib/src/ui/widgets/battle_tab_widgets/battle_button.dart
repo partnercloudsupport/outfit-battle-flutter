0ffc17b1851f54af6c4069c2ac573583b9b41639
herovickers <victoreronmosele@gmail.com>
2019-01-23 16:06:55 +0100

    resize dialog on battle tab

--- lib/src/ui/widgets/battle_tab_widgets/battle_button.dart
+++ lib/src/ui/widgets/battle_tab_widgets/battle_button.dart
@@ -2,42 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:outfit_battle/src/resources/custom_icons.dart';
 
 Expanded buildBattleButton(context) {
-    return Expanded(
-        flex: 2,
-        child: FloatingActionButton(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
-            child: Icon(
-              CustomIcons.swords_crossed,
-              color: Colors.white,
-            ),
+  return Expanded(
+    flex: 2,
+    child: Hero(
+      tag: 'test',
+      child: FloatingActionButton(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
+          child: Icon(
+            CustomIcons.swords_crossed,
+            color: Colors.white,
           ),
-          onPressed: () {
-            Navigator.push(context, CustomDialogRoute(
-              builder:(BuildContext context) {
-                return Center(child: AlertDialog( content: Container(height: 400, width: 50, child: Text("Upload screen"),),),);
-              },
-              ),
-                );
-
-          },
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            CustomDialogRoute(builder: (BuildContext context) {
+              return Center(
+                child: AlertDialog(
+                  content: Container(
+                    height: MediaQuery.of(context).size.height * .7,
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: Hero(
+                      tag: 'test',
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
+                          child: Icon(
+                            CustomIcons.swords_crossed,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-        );
-  }
+              );
+            }),
+          );
+        },
+      ),
+    ),
+  );
+}
 
-  class CustomDialogRoute<T> extends PageRoute<T> {
-    CustomDialogRoute({this.builder}) : super();
+class CustomDialogRoute<T> extends PageRoute<T> {
+  CustomDialogRoute({this.builder}) : super();
 
-    final WidgetBuilder builder;
+  final WidgetBuilder builder;
 
   @override
   bool get opaque => false;
 
   @override
   bool get barrierDismissible => true;
-  
 
   @override
   Color get barrierColor => Colors.black54;
@@ -47,17 +71,16 @@ Expanded buildBattleButton(context) {
   String get barrierLabel => null;
 
   @override
-  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
     return builder(context);
   }
 
   @override
-  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
     return FadeTransition(
-      opacity: CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeOut
-      ),
+      opacity: CurvedAnimation(parent: animation, curve: Curves.easeOut),
       child: child,
     );
   }
@@ -67,5 +90,4 @@ Expanded buildBattleButton(context) {
 
   @override
   Duration get transitionDuration => const Duration(milliseconds: 300);
-
-  }
+}
