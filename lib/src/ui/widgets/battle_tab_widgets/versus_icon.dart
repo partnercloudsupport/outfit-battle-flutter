f88c19b625bb5b90c0405ef18351c628b06dbc30
herovickers <victoreronmosele@gmail.com>
2019-01-23 05:54:17 +0100

    add timestamp to battles

--- lib/src/ui/widgets/battle_tab_widgets/versus_icon.dart
+++ lib/src/ui/widgets/battle_tab_widgets/versus_icon.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
 
-Expanded buildVersusIcon(){
-      return Expanded(
-        flex: 2,
-        child: Container(
-          constraints: BoxConstraints(
-            maxHeight: double.infinity,
-            maxWidth: double.infinity,
-            minWidth: double.infinity,
-            minHeight: double.infinity),
-              child: FractionallySizedBox(
-                alignment: Alignment(0.0, 0.0),
-                widthFactor: 0.5,
-                heightFactor: 0.5,
-                  child: Stack(
-                    alignment: Alignment.center,
-                      children: <Widget> [Container(
-                      decoration: new BoxDecoration(
-                        color: Colors.black87,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    Text("VS",
-                    style: TextStyle(color: Colors.white),),
-                  ],
-      ),      
+Expanded buildVersusIcon() {
+  return Expanded(
+    flex: 2,
+    child: Container(
+      constraints: BoxConstraints(
+          maxHeight: double.infinity,
+          maxWidth: double.infinity,
+          minWidth: double.infinity,
+          minHeight: double.infinity),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Spacer(),
+          Expanded(
+            flex: 2,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.black87,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Text(
+                  "VS",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 2,),
+          Expanded(
+            flex: 1,
+            child: Text("1 hr ago", style: TextStyle(color: Colors.black54),),
+          ),
+        ],
       ),
     ),
-    );
-  }
+  );
+}
